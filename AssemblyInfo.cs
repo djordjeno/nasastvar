1a75e74afa3b79071fc5a3f38ed0997c01fc623a
VrhovniStab\X <ns.djordje@gmail.com>
2016-11-27 20:52:42 +0100

    dodana dva fajla jos

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// Assembly GoloMedicaUtil, Version 1.0.0.0
+
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Runtime.InteropServices.Guid("31b547db-41d9-4cb4-a637-b2f765e792c7")]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9  2007")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Reflection.AssemblyProduct("GoloMedicaUtil")]
+[assembly: System.Reflection.AssemblyCompany("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyTitle("GoloMedicaUtil")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+
